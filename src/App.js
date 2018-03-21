66c567c840e28f7927802194f496a251286ebbc8
incorporealghost <incorporealghost@users.noreply.github.com>
2018-03-20 23:22:51 -0500

    added via web upload
    I am screwing this up in every possible way

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Search extends Component{
+    render() {
+        return (
+            <div className="Search">
+                <h1>Hello, World!</h1>
+            </div>
+        )
+    }
+}
+
+export default Search;
